ca6cfff1d3a911c2535a4c81e34f90ebc22096f8
sorban.ivan <sorban.ivan@D20-06>
2021-11-16 11:20:09 +0100

    ty hadas

--- hadanicisel_/Program.cs
+++ hadanicisel_/Program.cs
@@ -6,7 +6,33 @@ namespace hadanicisel_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool neuhodl = true;
+            int tipcislo;
+
+            Random random = new Random();
+            int nahcislo = random.Next(1, 100);
+
+            Console.WriteLine("hadani cisel");
+            Console.WriteLine("myslim si cislo od 1 do 100");
+
+            while (neuhodl)
+            {
+                Console.Write("napis svuj tip:");
+                tipcislo = int.Parse(Console.ReadLine());
+                if (tipcislo < nahcislo)
+                {
+                    Console.WriteLine("to je malo");
+                }
+                else if (tipcislo > nahcislo)
+                {
+                    Console.WriteLine("To je moc");
+                }
+                else
+                {
+                    Console.WriteLine("trefa!!!!!!!!");
+                    neuhodl = false;
+                }
+            }
         }
     }
 }
